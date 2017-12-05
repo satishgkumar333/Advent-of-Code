36ab8b6418a2fa719c1d86de0807d3af29d44604
Stephan van Hulst <s.van.hulst@b-ware.com>
2017-12-05 22:11:54 +0100

    Simplified turtle

--- src/aoc/y17/d3/ProblemTwo.java
+++ src/aoc/y17/d3/ProblemTwo.java
@@ -1,55 +1,56 @@
 package aoc.y17.d3;
 
 import java.util.*;
-import java.util.function.*;
 import java.util.stream.*;
 
 import static aoc.y17.d3.Rotation.*;
 
 final class ProblemTwo {
-
-  private final int input;
   
   private int lastComputation = 1;
+  
+  private final GrowingTwoWayMatrix<Integer> matrix = new GrowingTwoWayMatrix<>();
+  {
+    matrix.set(0, 0, lastComputation);
+  }
 
-  private final Turtle<Integer> turtle = new Turtle<>(Direction.RIGHT, lastComputation)
-    .setComputationPerCell(ProblemTwo::getSumOfAdjacentElements)
-    .addComputationListener(event -> updateLastComputation(event.newValue()));
+  private final Turtle turtle = new Turtle(Direction.DOWN).addMovementListener(event -> updateLastComputation());
+
+  private final int input;
 
   ProblemTwo(int input) {
     this.input = input;
   }
 
-  private void updateLastComputation(int computation) {
-    if (lastComputation < input)
-      lastComputation = computation;
+  private void updateLastComputation() {
+    if (lastComputation > input)
+      return;
+
+    lastComputation = getValuesAdjacentToTurtle().sum();
+    matrix.set(turtle.x(), turtle.y(), lastComputation);
   }
 
   int solve() {
     while (lastComputation < input) {
       turtle
-        .moveForward()
+        .incrementSpeed()
         .rotate(COUNTER_CLOCKWISE)
         .moveForward()
         .rotate(COUNTER_CLOCKWISE)
-        .incrementSpeed();
+        .moveForward();
     }
 
     return lastComputation;
   }
 
-  private static int getSumOfAdjacentElements(Turtle<Integer> turtle) {
-    return getElementsAdjacentTo(turtle.x(), turtle.y(), turtle.matrix()).mapToInt(Integer::intValue).sum();
-  }
-
-  private static <T> Stream<T> getElementsAdjacentTo(int x, int y, TwoWayMatrix<? extends T> matrix) {
+  private IntStream getValuesAdjacentToTurtle() {
     return IntStream.rangeClosed(-1,1)
-      .mapToObj(dy -> IntStream.rangeClosed(-1,1)
+      .flatMap(dy -> IntStream.rangeClosed(-1,1)
         .filter(dx -> dx != 0 || dy != 0)
-        .mapToObj(dx -> matrix.get(x+dx, y+dy)))
-      .flatMap(Function.identity())
-      .filter(Optional::isPresent)
-      .map(Optional::get);
+        .mapToObj(dx -> matrix.get(turtle.x()+dx, turtle.y()+dy))
+        .filter(Optional::isPresent)
+        .mapToInt(Optional::get)
+      );
   }
 
   public static void main(String... args) {
--- src/aoc/y17/d3/Turtle.java
+++ src/aoc/y17/d3/Turtle.java
@@ -3,27 +3,19 @@ package aoc.y17.d3;
 import java.util.*;
 import java.util.function.*;
 
-final class Turtle<T> {
+final class Turtle {
 
-  private int x, y;
-  
-  private int speed = 1;
+  private int x, y, speed;
 
   private Direction direction;
 
-  private Function<? super Turtle<T>, ? extends T> computationPerCell;
+  private final List<Consumer<? super Turtle>> movementListeners = new ArrayList<>();
 
-  private final GrowingTwoWayMatrix<T> matrix = new GrowingTwoWayMatrix<>();
-
-  private final List<ComputationListener<? super T>> computationListeners = new ArrayList<>();
-
-  Turtle(Direction direction, T startingCellValue) {
-    if (direction == null)
+  Turtle(Direction startingDirection) {
+    if (startingDirection == null)
       throw new IllegalArgumentException();
 
-    this.direction = direction;
-
-    matrix.set(x, y, startingCellValue);
+    direction = startingDirection;
   }
   
   int x() {
@@ -41,56 +33,38 @@ final class Turtle<T> {
   Direction direction() {
     return direction;
   }
-  
-  TwoWayMatrix<T> matrix() {
-    return matrix.asReadOnlyMatrix();
-  }
 
-  Turtle<T> incrementSpeed() {
+  Turtle incrementSpeed() {
     speed++;
     return this;
   }
   
-  Turtle<T> rotate(Rotation rotation) {
+  Turtle rotate(Rotation rotation) {
     direction = rotation.applyTo(direction);
     return this;
   }
 
-  Turtle<T> setComputationPerCell(Function<? super Turtle<T>, ? extends T> computationPerCell) {
-    this.computationPerCell = computationPerCell;
-    return this;
-  }
-
-  private void moveOneCellForward() {
-    switch (direction) {
-      case RIGHT : x++; break;
-      case DOWN  : y--; break;
-      case LEFT  : x--; break;
-      case UP    : y++; break;
-    }
-  }
-
-  Turtle<T> moveForward() {
+  Turtle moveForward() {
     for (int i = 0; i < speed; i++) {
-      moveOneCellForward();
-      
-      if (computationPerCell != null) {
-        T newValue = computationPerCell.apply(this);
-        Optional<T> oldValue = matrix.set(x, y, newValue);
-        fireComputationEvent(oldValue, newValue);
+      switch (direction) {
+        case RIGHT : x++; break;
+        case DOWN  : y--; break;
+        case LEFT  : x--; break;
+        case UP    : y++; break;
       }
+
+      fireMoveEvent();
     }
 
     return this;
   }
 
-  Turtle<T> addComputationListener(ComputationListener<? super T> listener) {
-    computationListeners.add(listener);
+  Turtle addMovementListener(Consumer<? super Turtle> listener) {
+    movementListeners.add(listener);
     return this;
   }
 
-  private void fireComputationEvent(Optional<T> oldValue, T newValue) {
-    ComputationEvent<T> event = new ComputationEvent<>(matrix.asReadOnlyMatrix(), x, y, oldValue, newValue);
-    computationListeners.forEach(listener -> listener.computationPerformed(event));
+  private void fireMoveEvent() {
+    movementListeners.forEach(listener -> listener.accept(this));
   }
 }
