75fc4da8c4a2f462bb6d25004552295e8bf4c0fa
Stephan van Hulst <s.van.hulst@b-ware.com>
2018-01-03 22:30:30 +0100

    Add dummy project for year 2018

---
+++ y18/src/main/java/nl/nibsi/aoc/y18/DayOne.java
@@ -0,0 +1,18 @@
+package nl.nibsi.aoc.y18;
+
+import java.io.*;
+
+import nl.nibsi.aoc.*;
+
+public final class DayOne implements Puzzle {
+
+  @Override
+  public String solveFirstPart(Reader inputReader) throws IOException {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  @Override
+  public String solveSecondPart(Reader inputReader) throws IOException {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}
---
+++ y18/src/main/java/nl/nibsi/aoc/y18/MyPuzzleProvider.java
@@ -0,0 +1,10 @@
+package nl.nibsi.aoc.y18;
+
+import nl.nibsi.aoc.provider.*;
+
+public final class MyPuzzleProvider extends QuickPuzzleProvider {
+
+  public MyPuzzleProvider() {
+    super(MyPuzzleProvider.class);
+  }
+}
