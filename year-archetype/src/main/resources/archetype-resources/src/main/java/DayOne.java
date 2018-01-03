26f885c5a39db31a6c9300a7a1213453d8a524d9
Stephan van Hulst <s.van.hulst@b-ware.com>
2018-01-04 00:08:19 +0100

    Add year archetype

---
+++ year-archetype/src/main/resources/archetype-resources/src/main/java/DayOne.java
@@ -0,0 +1,18 @@
+package ${package};
+
+import java.io.*;
+
+import nl.nibsi.aoc.*;
+
+public final class DayOne implements Puzzle {
+
+  @Override
+  public String solveFirstPart(Reader inputReader) throws IOException {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  @Override
+  public String solveSecondPart(Reader inputReader) throws IOException {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}
---
+++ year-archetype/src/main/resources/archetype-resources/src/main/java/MyPuzzleProvider.java
@@ -0,0 +1,10 @@
+package ${package};
+
+import nl.nibsi.aoc.provider.*;
+
+public final class MyPuzzleProvider extends QuickPuzzleProvider {
+
+  public MyPuzzleProvider() {
+    super(MyPuzzleProvider.class);
+  }
+}
